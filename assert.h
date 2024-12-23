3f958e5c9819b83e96df7e61a88ca8e1d6082ff7
user <user@mail.tld>
2024-12-23 05:13:22 +0100

    Added CLIBC_NO_ASSERT option

--- assert.h
+++ assert.h
@@ -2,6 +2,10 @@
 
 void panic(const char *message);
 
+#ifndef CLIBC_NO_ASSERT
 #define ASSERT_DETAIL_STRINGIZE_DETAIL(x) #x
 #define ASSERT_DETAIL_STRINGIZE(x) ASSERT_DETAIL_STRINGIZE_DETAIL(x)
 #define assert(condition) do {if (!(condition)) panic("Assertion failure (" ASSERT_DETAIL_STRINGIZE(__LINE__) "@" __FILE__ "): " #condition);} while (0)
+#else
+#define assert(condition) {(void)(condition);}
+#endif
